8d837b0fcebdf2d7f296ca1dc0e8a182db24fed9
Kevin Hernández <akevin.2215@gmail.com>
2025-07-12 15:35:02 +0200

    feat: add ProfileScreen component and integrate Firestore for routine management

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout() {
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="login" options={{ headerShown: false }} />
-      <Stack.Screen name="home" options={{ headerTitle: "Inicio" }} />
+      <Stack.Screen name="home" options={{ headerShown: false }} />
     </Stack>
   );
 }
